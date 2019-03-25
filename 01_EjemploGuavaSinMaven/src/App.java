c6d824690d7c4860b70154c1b86d4ed068dadff1
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2019-03-25 07:57:45 +0100

    First Commit

---
+++ 01_EjemploGuavaSinMaven/src/App.java
@@ -0,0 +1,18 @@
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		BiMap<Integer, String> empleados = HashBiMap.create();
+
+		empleados.put(new Integer(101), "Angel");
+		empleados.put(new Integer(102), "Luismi");
+		empleados.put(new Integer(103), "Miguel");
+
+		System.out.println(empleados.inverse().get("Angel"));
+
+	}
+
+}
