eeeec0eecda70db4cec3628d048a7efb633d17ba
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2019-04-22 11:56:36 +0200

    Solución de un pequeño error en data.sql

--- 11_CrudCompletoUnaEntidad/src/main/resources/data.sql
+++ 11_CrudCompletoUnaEntidad/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into alumno values (NEXTVAL('hibernate_sequence'),'Luis Miguel','López','luismi.lopez@triana.com');
-insert into alumno values (NEXTVAL('hibernate_sequence'),'Ángel','Naranjo','angel.narajo@triana.com');
+insert into alumno (id, nombre, apellidos, email) values (NEXTVAL('hibernate_sequence'),'Luis Miguel','López','luismi.lopez@triana.com');
+insert into alumno (id, nombre, apellidos, email) values (NEXTVAL('hibernate_sequence'),'Ángel','Naranjo','angel.narajo@triana.com');
