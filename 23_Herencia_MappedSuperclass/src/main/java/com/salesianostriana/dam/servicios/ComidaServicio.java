48ec3e615ae140db072d53675689b3d563383378
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2019-05-03 13:51:46 +0200

    Ejemplo de MappedSuperclass

---
+++ 23_Herencia_MappedSuperclass/src/main/java/com/salesianostriana/dam/servicios/ComidaServicio.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.salesianostriana.dam.servicios;
+
+import org.springframework.stereotype.Service;
+
+import com.salesianostriana.dam.modelo.Comida;
+import com.salesianostriana.dam.repositorios.ComidaRepository;
+import com.salesianostriana.dam.servicios.base.BaseService;
+
+/**
+ * @author luismi
+ *
+ */
+@Service
+public class ComidaServicio extends BaseService<Comida, Long, ComidaRepository>{
+
+}
