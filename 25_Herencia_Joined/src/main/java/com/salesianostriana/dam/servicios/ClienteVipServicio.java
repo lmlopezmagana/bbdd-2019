6b1e13263d8a7fe9b34836de94c3e754b8e09dc4
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2019-05-07 10:06:49 +0200

    Ejemplo 25 - Herencia JOINED

---
+++ 25_Herencia_Joined/src/main/java/com/salesianostriana/dam/servicios/ClienteVipServicio.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.salesianostriana.dam.servicios;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.salesianostriana.dam.modelo.ClienteVip;
+import com.salesianostriana.dam.repositorios.ClienteVipRepository;
+import com.salesianostriana.dam.servicios.base.BaseService;
+
+/**
+ * @author luismi
+ *
+ */
+@Service
+public class ClienteVipServicio extends BaseService<ClienteVip, Long, ClienteVipRepository>{
+
+	public List<ClienteVip> vipsPorFecha(LocalDate inicio, LocalDate fin) {
+		return repositorio.findByFechaVipBetween(inicio, fin);
+	}
+	
+}
